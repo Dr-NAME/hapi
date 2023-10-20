a99caf34e3f9b7d5c5300e87bf3a27b626c68829
Dr-NAME <clement.jacquemin1@etudiant.univ-reims.fr>
2023-10-20 18:31:43 +0200

    (fix) Move version header to __init__

--- hapi/__init__.py
+++ hapi/__init__.py
@@ -1,7 +1,24 @@
-#from hapi import *
+HAPI_VERSION = '1.2.2.1'; __version__ = HAPI_VERSION
 
-#https://docs.python.org/3/tutorial/modules.html
-#__all__ = ['hapi',]
+# version header
+print('HAPI version: %s' % HAPI_VERSION)
+print('To get the most up-to-date version please check http://hitran.org/hapi')
+print('ATTENTION: Python versions of partition sums from TIPS-2021 are now available in HAPI code')
+print('')
+print('           MIT license: Copyright 2021 HITRAN team, see more at http://hitran.org. ')
+print('')
+print('           If you use HAPI in your research or software development,')
+print('           please cite it using the following reference:')
+print('           R.V. Kochanov, I.E. Gordon, L.S. Rothman, P. Wcislo, C. Hill, J.S. Wilzewski,')
+print('           HITRAN Application Programming Interface (HAPI): A comprehensive approach')
+print('           to working with spectroscopic data, J. Quant. Spectrosc. Radiat. Transfer 177, 15-30 (2016)')
+print('           DOI: 10.1016/j.jqsrt.2016.03.005')
+print('')
+print('           ATTENTION: This is the core version of the HITRAN Application Programming Interface.')
+print('                      For more efficient implementation of the absorption coefficient routine, ')
+print('                      as well as for new profiles, parameters and other functional,')
+print('                      please consider using HAPI2 extension library.')
+print('                      HAPI2 package is available at http://github.com/hitranonline/hapi2')
+print('')
 
-#https://stackoverflow.com/questions/34341420/import-functions-directly-from-python-3-modules
 from .hapi import *
