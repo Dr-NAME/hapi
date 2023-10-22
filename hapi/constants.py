991dac86604a392e4f25ab2383f3526c7d1735fe
Dr-NAME <clement.jacquemin1@etudiant.univ-reims.fr>
2023-10-22 10:28:08 +0200

    (move) physical and numerical constants

---
+++ hapi/constants.py
@@ -0,0 +1,15 @@
+from .dtype import *
+
+# define zero
+cZero = __FloatType__(0.)
+
+# physical constants
+cBolts = 1.380648813E-16 # erg/K, CGS
+cc = 2.99792458e10 # cm/s, CGS
+hh = 6.626196e-27 # erg*s, CGS
+
+# computational constants
+cSqrtLn2divSqrtPi = 0.469718639319144059835
+cLn2 = 0.6931471805599
+cSqrtLn2 = 0.8325546111577
+cSqrt2Ln2 = 1.1774100225
--- hapi/hapi.py
+++ hapi/hapi.py
@@ -39,6 +39,8 @@ from time import time
 import pydoc
 from .tips import *
 from .dtype import *
+from .constants import *
+
 # Enable warning repetitions
 simplefilter('always', UserWarning)
 
@@ -54,21 +56,6 @@ if 'io' in sys.modules: # define open using Linux-style line endings
         return io.open(*args,**argv)
 else:
     open_ = open
-
-
-# define zero
-cZero = __FloatType__(0.)
-
-# physical constants
-cBolts = 1.380648813E-16 # erg/K, CGS
-cc = 2.99792458e10 # cm/s, CGS
-hh = 6.626196e-27 # erg*s, CGS
-
-# computational constants
-cSqrtLn2divSqrtPi = 0.469718639319144059835
-cLn2 = 0.6931471805599
-cSqrtLn2 = 0.8325546111577
-cSqrt2Ln2 = 1.1774100225
 
 # initialize global variables
 VARIABLES = {}
